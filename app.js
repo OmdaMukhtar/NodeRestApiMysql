ed848187463c49f1d04d47bdb42d5d8fa8f8284d
omda <abuoop123@gmail.com>
2019-07-27 23:57:27 +0200

    add root router

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.use(morgan('short'));
 app.use(router);
 app.use('/public', express.static('public/views'))
 
-
+app.get('/', ()=>{
+    resizeBy.send("Hello guys welcome to our root nodeJs");
+});
 
 app.listen('3003', ()=>{
     console.log("server express listen to the port 3000");
